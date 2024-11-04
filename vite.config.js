9d8571d15430fea9955d124e16c2b384a8d2a1ae
Leonel Javier Correa <leoneljaviercorrea.19@gmail.com>
2024-11-04 15:08:31 -0300

    Probando

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default {
+    server: {
+        port: "2222",
+    },
+    css: {
+        devSourcemap: true,
+    },
+    build: {
+        emptyOutDir: true,
+        rollupOptions: {
+        input: {
+            avanzado: resolve('views/avanzado.html'),
+            donacion: resolve('views/donacion.html'),
+            foro: resolve('views/foro.html'),
+            principiantes: resolve('views/nosotros.html'),
+            inicio: resolve('index.html')
+        }
+        }
+    }
+
+
+};
