e9f703d3c6cd6f53255b1a41a0b5f788cf8031ae
Leonel Javier Correa <leoneljaviercorrea.19@gmail.com>
2024-11-29 02:16:58 -0300

    Relleno informacion sobre formaciones y actualizacion de diseño para 'Avanzado'

--- javascript/info-licenciaturas.js
+++ javascript/info-licenciaturas.js
@@ -1,115 +1,180 @@
 export const arrayFormacionLicenciatura = [
     {
-        id: 1, titulo_carrera: 'Licenciatura 1', 
-        informacion_de_la_carrera: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci exercitationem fuga aliquam rerum minus aliquid consectetur, incidunt in nostrum et, sunt eligendi corporis architecto asperiores eveniet quod numquam repellendus molestias iusto temporibus deserunt beatae? Cupiditate aspernatur dolorem quos laborum delectus, sint enim, sit sequi voluptatibus sapiente vitae ad reiciendis laboriosam!',
+        id: 1, titulo_carrera: 'Ciencias de la Computación', 
+        informacion_de_la_carrera: 'Es una disciplina que estudia los fundamentos teóricos y prácticos de la computación, abarcando algoritmos, estructuras de datos, inteligencia artificial, sistemas operativos, redes y desarrollo de software.',
         instituciones: [
             {
-                url: '#',
-                nombre_instituto: '- Nombre de la facultad',
-                mensaje: 'Gratuita - modalidad presencial y virtual'
+                url: 'https://exactas.uba.ar/ensenanza/carreras-de-grado/ciencias-de-la-computacion/',
+                nombre_instituto: '- UBA',
+                mensaje: 'Gratuita - modalidad presencial'
             },
             {
-                url: '#',
-                nombre_instituto: '- Nombre de la facultad 1',
+                url: 'https://vocaciones.fundacionypf.org.ar/ciencias-de-la-computacion.html',
+                nombre_instituto: '- Fundación YPF LAB',
+                mensaje: 'Gratuita - modalidad hibrida'
+            },
+            {
+                url: 'https://web.fceia.unr.edu.ar/es/licenciatura-en-ciencias-de-la-computaci%C3%B3n.html',
+                nombre_instituto: '- Universidad Nacional de Rosario',
+                mensaje: 'Gratuita - modalidad presencial'
+            },
+            {
+                url: 'https://www.uncuyo.edu.ar/estudios/carrera/licenciatura-en-ciencias-de-la-computacion',
+                nombre_instituto: '- Universidad Nacional de Cuyo',
+                mensaje: 'Gratuita - modalidad presencial'
+            }
+        ]
+    },
+    {
+        id: 2, titulo_carrera: 'Producción de Simuladores y Videojuegos', 
+        informacion_de_la_carrera: 'Es una carrera enfocada en diseñar, desarrollar y gestionar simuladores y videojuegos, combinando programación, diseño gráfico, narrativa interactiva, inteligencia artificial y herramientas tecnológicas avanzadas.',
+        instituciones: [
+            {
+                url: 'https://uai.edu.ar/facultades/tecnolog%C3%ADa-inform%C3%A1tica/licenciatura-en-producci%C3%B3n-de-simuladores-y-videojuegos/',
+                nombre_instituto: '- Universidad Abierta Interamericana (UAI)',
+                mensaje: 'Pago - modalidad presencial'
+            },
+            {
+                url: 'https://udelaciudad.edu.ar/licenciatura-en-desarrollo-y-produccion-de-videojuegos/',
+                nombre_instituto: '- Universidad de la Ciudad de Buenos Aires',
+                mensaje: 'Gratuita - modalidad presencial'
+            },
+            {
+                url: 'https://www.uade.edu.ar/facultad-de-ingenieria-y-ciencias-exactas/licenciatura-en-desarrollo-de-videojuegos/',
+                nombre_instituto: '- UADE',
+                mensaje: 'Pago - modalidad presencial'
+            },
+            {
+                url: 'https://www.ucongreso.edu.ar/carrera/desarrollo-de-simulaciones-virtuales-y-videojuegos/',
+                nombre_instituto: '- Universidad de Congreso (UC)',
+                mensaje: 'Pago - modalidad presencial'
+            }
+        ]
+    },
+    {
+        id: 3, titulo_carrera: 'Lic. en Análisis de Sistemas', 
+        informacion_de_la_carrera: 'Es una carrera que forma profesionales en el diseño, desarrollo, implementación y gestión de sistemas informáticos para resolver problemas organizacionales, integrando conocimientos de programación, bases de datos, análisis funcional y gestión de proyectos.',
+        instituciones: [
+            {
+                url: 'https://www.fi.uba.ar/grado/carreras/lic-en-analisis-de-sistemas',
+                nombre_instituto: '- UBA',
+                mensaje: 'Gratuita - modalidad hibrida'
+            },
+            {
+                url: 'https://www.ucaece.edu.ar/es/carrera/licenciatura-en-sistemas',
+                nombre_instituto: '- CAECE',
+                mensaje: 'Pago - modalidad hibrida'
+            },
+            {
+                url: 'https://www.info.unlp.edu.ar/licenciatura-en-sistemas/',
+                nombre_instituto: '- Universidad Nacional de La Plata (UNLP)',
+                mensaje: 'Graruita - modalidad hibrida'
+            }
+        ]
+    },
+    {
+        id: 4, titulo_carrera: 'Lic. en Gestión de la Tecnología', 
+        informacion_de_la_carrera: 'Forma profesionales para liderar y gestionar proyectos tecnológicos, integrando conocimientos en innovación, administración, transformación digital y estrategias de tecnología en organizaciones.',
+        instituciones: [
+            {
+                url: 'https://www.uade.edu.ar/facultad-de-ingenieria-y-ciencias-exactas/licenciatura-en-gestion-de-tecnologia-de-la-informacion/',
+                nombre_instituto: '- UADE',
+                mensaje: 'Pago - modalidad hibrida'
+            },
+            {
+                url: 'https://formacioncontinua.unlam.edu.ar/index.php?seccion=2&idArticulo=54',
+                nombre_instituto: '- UNLaM',
+                mensaje: 'Pago - modalidad hibrida'
+            },
+            {
+                url: 'https://uai.edu.ar/facultades/tecnolog%C3%ADa-inform%C3%A1tica/licenciatura-en-gesti%C3%B3n-de-tecnolog%C3%ADa-inform%C3%A1tica/',
+                nombre_instituto: '- Universidad Abierta Interamericana (UAI)',
+                mensaje: 'Pago - modalidad presencial'
+            },
+            {
+                url: 'https://www.unpaz.edu.ar/gestiontecnologias?language_content_entity=es',
+                nombre_instituto: '- UNPAZ',
+                mensaje: 'Pago - modalidad presencial'
+            },
+            {
+                url: 'https://www.unraf.edu.ar/carreras-unraf/licenciaturas-ingenierias/155-propuesta/lic/pres/2866-licenciatura-en-gestion-de-la-tecnologia-2',
+                nombre_instituto: '- Universidad Nacional de Rafaela (UNRaf)',
+                mensaje: 'Gratuita/Pago - modalidad presencial'
+            },
+            {
+                url: 'https://www.itba.edu.ar/grado/licenciatura-en-negocios-y-tecnologia/',
+                nombre_instituto: '- Institu tecnológico de Buenos Aires',
+                mensaje: 'Pago - modalidad presencial'
+            }
+        ]
+    },
+    {
+        id: 5, titulo_carrera: 'Lic. en Sistemas de Información', 
+        informacion_de_la_carrera: 'Prepara expertos en analizar, diseñar, implementar y gestionar sistemas informáticos para optimizar procesos y toma de decisiones en organizaciones, combinando tecnología y gestión empresarial.',
+        instituciones: [
+            {
+                url: 'https://fce.unse.edu.ar/?q=licenciatura-en-sistemas-de-informacion',
+                nombre_instituto: '- Universidad Nacional de Santiago del Estero',
+                mensaje: 'Gratuita - modalidad hibrida'
+            },
+            {
+                url: 'https://www.unlu.edu.ar/carg-sistemas.html',
+                nombre_instituto: '- Universidad Nacional de Lujan',
+                mensaje: 'Gratuita - modalidad Presencial'
+            },
+            {
+                url: 'https://exa.unne.edu.ar/r/?page_id=8390',
+                nombre_instituto: '- Universidad Nacional del Nordeste',
+                mensaje: 'Gratuita - modalidad Presencial'
+            },
+            {
+                url: 'https://www.uch.edu.ar/carrera/licenciatura-en-sistemas-de-informacion',
+                nombre_instituto: '- Universidad Champagnat',
+                mensaje: 'Paga - modalidad Presencial'
+            }
+        ]
+    },
+    {
+        id: 6, titulo_carrera: 'Lic. en Inteligencia Artificial', 
+        informacion_de_la_carrera: 'Forma especialistas en diseñar, desarrollar y aplicar sistemas de IA, abordando áreas como aprendizaje automático, procesamiento de lenguaje natural, visión por computadora y ética tecnológica.',
+        instituciones: [
+            {
+                url: 'https://www.palermo.edu/ingenieria/licenciatura-inteligencia-artificial/',
+                nombre_instituto: '- Universidad de Palermo (UP)',
                 mensaje: 'Paga - modalidad presencial'
             },
             {
-                url: '#',
-                nombre_instituto: '- Nombre de la facultad 2',
-                mensaje: 'Gratuita - modalidad virtual'
+                url: 'https://www.21.edu.ar/oferta-academica/licenciatura-en-inteligencia-artificial-y-robotica',
+                nombre_instituto: '- Universidad Siglo 21',
+                mensaje: 'Paga - modalidad presencial'
             },
             {
-                url: '#',
-                nombre_instituto: '- Nombre de la facultad 3',
-                mensaje: 'Gratuita - modalidad presencial'
-            },
-            {
-                url: '#',
-                nombre_instituto: '- Nombre de la facultad 4',
-                mensaje: 'Gratuita - modalidad presencial'
-            },
-            {
-                url: '#',
-                nombre_instituto: '- Nombre de la facultad 5',
+                url: 'https://www.ingenieria.uba.ar/posgrado/carreras-de-especializacion/inteligencia-artificial/plan-de-estudios',
+                nombre_instituto: '- UBA',
                 mensaje: 'Gratuita - modalidad presencial'
             }
         ]
     },
     {
-        id: 2, titulo_carrera: 'Licenciatura 2', 
-        informacion_de_la_carrera: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci exercitationem fuga aliquam rerum minus aliquid consectetur, incidunt in nostrum et, sunt eligendi corporis architecto asperiores eveniet quod numquam repellendus molestias iusto temporibus deserunt beatae? Cupiditate aspernatur dolorem quos laborum delectus, sint enim, sit sequi voluptatibus sapiente vitae ad reiciendis laboriosam!',
+        id: 7, titulo_carrera: 'Lic. en Ciberseguridad', 
+        informacion_de_la_carrera: 'Prepara profesionales para proteger sistemas, redes y datos frente a amenazas digitales, abarcando criptografía, gestión de riesgos, seguridad en redes y respuesta a incidentes.',
         instituciones: [
             {
-                url: 'https://utn.edu.ar/es/estudiar-utn?tipo_busqueda=carreras&id_tipos_carreras=1&id_seleccion=8',
-                nombre_instituto: '- Universidad Tecnologica Nacional (UTN)',
+                url: 'https://www.ufasta.edu.ar/distancia/lic-ciberseguridad/',
+                nombre_instituto: '- Universidad FASTA',
                 mensaje: 'Pago - modalidad hibrida'
-            }
-        ]
-    },
-    {
-        id: 3, titulo_carrera: 'Licenciatura 3', 
-        informacion_de_la_carrera: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci exercitationem fuga aliquam rerum minus aliquid consectetur, incidunt in nostrum et, sunt eligendi corporis architecto asperiores eveniet quod numquam repellendus molestias iusto temporibus deserunt beatae? Cupiditate aspernatur dolorem quos laborum delectus, sint enim, sit sequi voluptatibus sapiente vitae ad reiciendis laboriosam!',
-        instituciones: [
+            },
             {
-                url: 'https://utn.edu.ar/es/estudiar-utn?tipo_busqueda=carreras&id_tipos_carreras=1&id_seleccion=8',
-                nombre_instituto: '- Universidad Tecnologica Nacional (UTN)',
+                url: 'https://www.palermo.edu/ingenieria/licenciatura-en-ciberseguridad/',
+                nombre_instituto: '- Universidad de Palermo (UP)',
                 mensaje: 'Pago - modalidad hibrida'
-            }
-        ]
-    },
-    {
-        id: 4, titulo_carrera: 'Licenciatura 4', 
-        informacion_de_la_carrera: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci exercitationem fuga aliquam rerum minus aliquid consectetur, incidunt in nostrum et, sunt eligendi corporis architecto asperiores eveniet quod numquam repellendus molestias iusto temporibus deserunt beatae? Cupiditate aspernatur dolorem quos laborum delectus, sint enim, sit sequi voluptatibus sapiente vitae ad reiciendis laboriosam!',
-        instituciones: [
+            },
             {
-                url: 'https://utn.edu.ar/es/estudiar-utn?tipo_busqueda=carreras&id_tipos_carreras=1&id_seleccion=8',
-                nombre_instituto: '- Universidad Tecnologica Nacional (UTN)',
-                mensaje: 'Pago - modalidad hibrida'
-            }
-        ]
-    },
-    {
-        id: 5, titulo_carrera: 'Licenciatura 5', 
-        informacion_de_la_carrera: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci exercitationem fuga aliquam rerum minus aliquid consectetur, incidunt in nostrum et, sunt eligendi corporis architecto asperiores eveniet quod numquam repellendus molestias iusto temporibus deserunt beatae? Cupiditate aspernatur dolorem quos laborum delectus, sint enim, sit sequi voluptatibus sapiente vitae ad reiciendis laboriosam!',
-        instituciones: [
-            {
-                url: 'https://utn.edu.ar/es/estudiar-utn?tipo_busqueda=carreras&id_tipos_carreras=1&id_seleccion=8',
-                nombre_instituto: '- Universidad Tecnologica Nacional (UTN)',
-                mensaje: 'Pago - modalidad hibrida'
-            }
-        ]
-    },
-    {
-        id: 6, titulo_carrera: 'Licenciatura 6', 
-        informacion_de_la_carrera: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci exercitationem fuga aliquam rerum minus aliquid consectetur, incidunt in nostrum et, sunt eligendi corporis architecto asperiores eveniet quod numquam repellendus molestias iusto temporibus deserunt beatae? Cupiditate aspernatur dolorem quos laborum delectus, sint enim, sit sequi voluptatibus sapiente vitae ad reiciendis laboriosam!',
-        instituciones: [
-            {
-                url: 'https://utn.edu.ar/es/estudiar-utn?tipo_busqueda=carreras&id_tipos_carreras=1&id_seleccion=8',
-                nombre_instituto: '- Universidad Tecnologica Nacional (UTN)',
-                mensaje: 'Gratuita - modalidad hibrida'
-            }
-        ]
-    },
-    {
-        id: 7, titulo_carrera: 'Licenciatura 7', 
-        informacion_de_la_carrera: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci exercitationem fuga aliquam rerum minus aliquid consectetur, incidunt in nostrum et, sunt eligendi corporis architecto asperiores eveniet quod numquam repellendus molestias iusto temporibus deserunt beatae? Cupiditate aspernatur dolorem quos laborum delectus, sint enim, sit sequi voluptatibus sapiente vitae ad reiciendis laboriosam!',
-        instituciones: [
-            {
-                url: 'https://utn.edu.ar/es/estudiar-utn?tipo_busqueda=carreras&id_tipos_carreras=1&id_seleccion=8',
-                nombre_instituto: '- Universidad Tecnologica Nacional (UTN)',
-                mensaje: 'Pago - modalidad hibrida'
-            }
-        ]
-    },
-    {
-        id: 8, titulo_carrera: 'Licenciatura 8', 
-        informacion_de_la_carrera: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci exercitationem fuga aliquam rerum minus aliquid consectetur, incidunt in nostrum et, sunt eligendi corporis architecto asperiores eveniet quod numquam repellendus molestias iusto temporibus deserunt beatae? Cupiditate aspernatur dolorem quos laborum delectus, sint enim, sit sequi voluptatibus sapiente vitae ad reiciendis laboriosam!',
-        instituciones: [
-            {
-                url: 'https://utn.edu.ar/es/estudiar-utn?tipo_busqueda=carreras&id_tipos_carreras=1&id_seleccion=8',
-                nombre_instituto: '- Universidad Tecnologica Nacional (UTN)',
+                url: 'https://21.edu.ar/oferta-academica/licenciatura-en-seguridad-informatica',
+                nombre_instituto: '- Universidad Siglo 21',
                 mensaje: 'Pago - modalidad hibrida'
             }
         ]
     }
+    
 ]
